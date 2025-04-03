e9e9f9163fe29c0b732561714f3c2b0f94ae0f7b
Jorge Pita <jpitam@uoc.edu>
2025-04-03 23:30:11 +0200

    Corrección del link a recetas en index, corrección de duplicidad en index.js y mejora responsive añadiendo menu hamburguesa para versiones moviles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('DOMContentLoaded', function () {
     const boton = document.getElementById('mensaje-btn');
     if (boton) {
       boton.addEventListener('click', function () {
         alert('El secreto es que tenía que añadir alguna funcionalidad en js y no sabía que hacer.');
+      });
+    }
+  
+    const toggle = document.getElementById('menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+  
+    if (toggle && navMenu) {
+      toggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
       });
     }
   });
